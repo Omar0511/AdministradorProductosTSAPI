c1e121e741125deacaf6e3a6a2134a0a9384a72d
Omar <omar.garcia@morsa.com.mx>
2025-12-29 11:40:53 -0600

    Probando VALIDACION de CREAR PRODUCTO

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -2,6 +2,21 @@ import request from "supertest";
 import server from "../../server";
 
 describe("POST /api/products", () => {
+  it("should display validation errors", async () => {
+    const response = await request(server)
+      .post("/api/products")
+      // send: es lo que vamos a pasarle
+      .send({
+      });
+    
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('errors');
+    expect(response.body.errors).toHaveLength(4);
+
+    expect(response.status).not.toBe(404);
+    expect(response.body.errors).not.toHaveLength(2);
+  });
+  
   it("should create a new product", async () => {
     const response = await request(server)
       .post("/api/products")
@@ -15,7 +30,7 @@ describe("POST /api/products", () => {
     expect(response.body).toHaveProperty('data');
     expect(response.status).not.toBe(404);
     expect(response.status).not.toBe(200);
-    expect(response.body).not.toHaveProperty('error');
+    expect(response.body).not.toHaveProperty('errors');
 
   });
 });
